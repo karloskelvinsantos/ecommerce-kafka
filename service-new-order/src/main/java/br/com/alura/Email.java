4bff33e4f509dbb08199bc66266d783889e273ef
Karlos Kelvin <karloskelvinsantos@gmail.com>
2022-01-13 17:07:19 -0300

    Configure modules in maven project

---
+++ service-new-order/src/main/java/br/com/alura/Email.java
@@ -0,0 +1,10 @@
+package br.com.alura;
+
+public class Email {
+    private final String subject, body;
+
+    public Email(String email, String body) {
+        this.subject = email;
+        this.body = body;
+    }
+}
